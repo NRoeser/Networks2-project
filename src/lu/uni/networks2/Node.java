36bb867c677a5e460a58a5d398cdf93634c58860
Emanuel Dias <emanuel.dias.001@student.uni.lu>
2021-04-12 17:38:37 +0200

    start of query&node

--- src/lu/uni/networks2/Node.java
+++ src/lu/uni/networks2/Node.java
@@ -1,5 +1,20 @@
 package lu.uni.networks2;
 
+import java.util.ArrayList;
+
 public class Node {
 
+	
+	private ArrayList<Node> connectedNodes = new ArrayList<Node>();
+	
+	
+	public void setQuery(Query q) {
+		
+	}
+	
+	public void getQuery(Query q) {
+		
+	}
+	
+	
 }
