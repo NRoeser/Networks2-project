e57b4e645d14814d5c67a9d756d947634d80cb42
NRoeser <nickroeser@hotmail.de>
2021-04-12 17:37:26 +0200

    Started with the client class

---
+++ src/lu/uni/networks2/Client.java
@@ -0,0 +1,46 @@
+package lu.uni.networks2;
+
+public class Client {
+	private String connectedNodeIP = null;
+	private String ip;
+	
+	public Client(String s) {
+		ip = s;
+	}
+	
+	public void askUser() {
+		boolean ongoing = true;
+		while (ongoing) {
+			
+		}
+	}
+	
+	public void setQuery(String key, String value) {
+		
+	}
+	
+	public void getQuery(String key) {
+		
+	}
+	
+	private void connect(String ip) {
+		if(connectedNodeIP == null) {
+			connectedNodeIP = ip;
+			System.out.println("Now connected to: " + ip);
+		} else {
+			System.out.println("Already connected to a node, needs to be disconnected first");
+		}
+	}
+	
+	private void disconnect() {
+		if (connectedNodeIP == null) {
+			System.out.println("Not connected to any node");
+		} else {
+			connectedNodeIP = null;
+			System.out.println("Now disconnected from former node");
+		}
+	}
+
+	
+
+}
